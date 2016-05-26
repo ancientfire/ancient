825c5c214e519eed10ecc5744f805198917efa3d
Agata Ciesielska <Agata Ciesielska>
2016-05-26 13:38:25 +0200

    .

---
+++ template/header.php
@@ -0,0 +1,62 @@
+<?php
+echo '
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Witaj | Welcome</title>
+    <link rel="stylesheet" href="foundation.css">
+    <link rel="stylesheet" href="foundation.min.css">
+    <link href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+    
+	<script src="jquery.js"></script>
+	<script src="what-input.js"></script>
+    <script src="foundation.js"></script>
+
+    <script src="foundation-datepicker.js"></script>
+    <script src="foundation-datepicker.vi.js"></script>
+    <link rel="stylesheet" href="foundation-datepicker.min.css">
+    <link rel="stylesheet" href="app.css">
+    
+    <link rel="shortcut icon" href="favicon.png" />
+
+
+</head>
+
+<body>
+        
+<div class="top-bar">
+    <div class="top-bar-left">
+        <ul class="dropdown menu" data-dropdown-menu>
+            <li class="menu-text">Hotel Project</li>
+            <li><a href="#">Strona główna</a></li>
+            ';
+            if($_SESSION['valid']==true){
+				echo '
+              <li>
+				<a href="#">Twoje konto</a>
+				<ul class="menu">
+					<li><a href="#">Item 1A</a></li>
+				<!-- ... -->
+				</ul>
+				</li>';
+			}
+			echo '
+			
+            <li><a href="#">Aktualne rezerwacje</a></li>
+            <li><a href="#">Zarezerwuj</a></li>
+            <li><a href="#">Historia</a></li>
+        </ul>
+    </div>';
+    if($_SESSION['valid']==true){
+		echo '
+    <div class="top-bar-right ">
+        <ul class="menu">
+            <li><a href="#">Wyloguj się</a></li>
+        </ul>
+    </div>';
+}
+
+echo '</div>';
+?>
