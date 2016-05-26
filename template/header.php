b25b3f4ff7092dfdf98eddef7229320000662652
Agata Ciesielska <Agata Ciesielska>
2016-05-26 21:03:04 +0200

    .

--- template/header.php
+++ template/header.php
@@ -27,12 +27,33 @@ echo '
 <body>
         
 <div class="top-bar">
-    <div class="top-bar-left">
+    <div class="top-bar-left">';
+    if(!$_SESSION['valid']){
+		echo '
         <ul class="menu">
             <li class="menu-text">Hotel Project</li>
-            <li><a href="#">Strona główna</a></li>
-            <li><a href="#">O hotelu</a></li>
-        </ul>
+            <li><a href="?">Strona główna</a></li>
+            <li><a href="?s=ohotelu">O hotelu</a></li>
+        </ul>';
+	}else{
+        echo '
+        <ul class="dropdown menu" data-dropdown-menu>
+            <li class="menu-text">Hotel Project</li>
+            <li><a href="?">Strona główna</a></li>
+              <li>
+				<a href="#">Twoje konto</a>
+				<ul class="menu">
+					<li><a href="#">Twoje dane</a></li>
+                    <li><a href="#">Edytuj dane</a></li>
+                    <li><a href="#">Zmień hasło</a></li>
+				</ul>
+			</li>
+            <li><a href="#">Aktualne rezerwacje</a></li>
+            <li><a href="#">Zarezerwuj</a></li>
+            <li><a href="#">Historia</a></li>
+        </ul>';}
+        
+        echo'
     </div>
     <div class="top-bar-right ">
     <ul class="menu">
@@ -43,7 +64,7 @@ echo '
     
         
             <li class="active"><a href="?s=logowanie">Zaloguj się</a></li>
-            <li><a href="#">Zarejestruj się</a></li>
+            <li><a href="?s=rejestracja">Zarejestruj się</a></li>
         
     ';
 }else{
