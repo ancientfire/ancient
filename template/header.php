94396376a201881c8dfadf3dee8ef8cd6e66aaa0
Agata Ciesielska <Agata Ciesielska>
2016-05-26 15:53:38 +0200

    .

--- template/header.php
+++ template/header.php
@@ -6,18 +6,18 @@ echo '
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>Witaj | Welcome</title>
-    <link rel="stylesheet" href="foundation.css">
-    <link rel="stylesheet" href="foundation.min.css">
+    <link rel="stylesheet" href="css/foundation.css">
+    <link rel="stylesheet" href="css/foundation.min.css">
     <link href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
     
-	<script src="jquery.js"></script>
-	<script src="what-input.js"></script>
-    <script src="foundation.js"></script>
+	<script src="js/jquery.js"></script>
+	<script src="js/what-input.js"></script>
+    <script src="js/foundation.js"></script>
 
-    <script src="foundation-datepicker.js"></script>
-    <script src="foundation-datepicker.vi.js"></script>
-    <link rel="stylesheet" href="foundation-datepicker.min.css">
-    <link rel="stylesheet" href="app.css">
+    <script src="js/foundation-datepicker.js"></script>
+    <script src="js/foundation-datepicker.vi.js"></script>
+    <link rel="stylesheet" href="css/foundation-datepicker.min.css">
+    <link rel="stylesheet" href="css/app.css">
     
     <link rel="shortcut icon" href="favicon.png" />
 
