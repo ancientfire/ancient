998abd724d7683439b194214f9986eb1bfd39137
Agata Ciesielska <Agata Ciesielska>
2016-05-28 21:25:48 +0200

    K

---
+++ REJ_PRAC.php
@@ -0,0 +1,66 @@
+<div class="primary callout">
+    <div class="row">
+        <div class="row large-12">
+
+            <label><strong>DANE PRACOWNIKA</strong></label>
+
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>Imię
+                        <input type="text" placeholder="Imię" /></th>
+
+                    <th>Nazwisko
+                        <input type="text" placeholder="Nazwisko" /></th>
+
+                    <th>PESEL
+                        <input type="text" placeholder="PESEL" /></th>
+
+                    <th>Numer telefonu
+                        <input type="text" placeholder="Numer telefonu" /></th>
+                </tr>
+                </thead>
+
+                <thead>
+                <tr>
+                    <th>Miasto
+                        <input type="text" placeholder="Miasto" /></th>
+
+                    <th>Ulica
+                        <input type="text" placeholder="Ulica" /></th>
+
+                    <th>Kod pocztowy
+                        <input type="text" placeholder="Kod pocztowy" /></th>
+
+                    <th>Numer lokalu
+                        <input type="text" placeholder="Numer lokalu" /></th>
+                    <th>Numer mieszkania
+                        <input type="text" placeholder="Numer mieszkania" /></th>
+                </tr>
+                </thead>
+            </table>
+            <div class="large-6">
+                <table class="table">
+                    <label><strong>DANE LOGOWANIA</strong></label>
+                    <thead>
+                    <tr>
+                        <th>E-mail
+                            <input type="text" placeholder="E-mail" /></th>
+                        <th>Hasło
+                            <input type="text" placeholder="Hasło" /></th>
+                        <th>Powtórz hasło
+                            <input type="text" placeholder="Hasło" /></th>
+                    </tr>
+                    </thead>
+
+                </table>
+
+                <div class="large-12">
+                    <a href="#" class="button rejestracja2">ZAREJESTRUJ PRACOWNIKA</a>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+</div>
