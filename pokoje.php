60b8599adaad39b4bfa3ce747e029dbe7ecbf321
Agata Ciesielska <Agata Ciesielska>
2016-05-29 11:34:49 +0200

    k

---
+++ pokoje.php
@@ -0,0 +1,113 @@
+<div class="primary callout archive">
+    <div class="row large-10">
+        <h3><strong>Baza pracowników</strong></h3>
+        <table>
+            <thead>
+            <tr>
+                <th width="100">ID pokoju</th>
+                <th width="100">Piętro</th>
+                <th width="100">Ilość osób</th>
+                <th width="100">Stan techniczny</th>
+
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>Content Here</td>
+                <td>Content Here</td>
+                <td>Content Here</td>
+                <td>Content Here</td>
+            </tr>
+            <tr>
+                <td>Content Here</td>
+                <td>Content Here</td>
+                <td>Content Here</td>
+                <td>Content Here</td>
+            </tr>
+            <tr>
+                <td>Content Here</td>
+                <td>Content Here</td>
+                <td>Content Here</td>
+                <td>Content Here</td>
+            </tr>
+
+            <tr>
+                <td>Content Here</td>
+                <td>Content Here</td>
+                <td>Content Here</td>
+                <td>Content Here</td>
+            </tr>
+            <tr>
+                <td>Content Here</td>
+                <td>Content Here</td>
+                <td>Content Here</td>
+                <td>Content Here</td>
+            </tr>
+            <tr>
+                <td>Content Here</td>
+                <td>Content Here</td>
+                <td>Content Here</td>
+                <td>Content Here</td>
+            </tr>
+
+            <tr>
+                <td>Content Here</td>
+                <td>Content Here</td>
+                <td>Content Here</td>
+                <td>Content Here</td>
+            </tr>
+            <tr>
+                <td>Content Here</td>
+                <td>Content Here</td>
+                <td>Content Here</td>
+                <td>Content Here</td>
+            </tr>
+            <tr>
+                <td>Content Here</td>
+                <td>Content Here</td>
+                <td>Content Here</td>
+                <td>Content Here</td>
+            </tr>
+
+            <tr>
+                <td>Content Here</td>
+                <td>Content Here</td>
+                <td>Content Here</td>
+                <td>Content Here</td>
+            </tr>
+            <tr>
+                <td>Content Here</td>
+                <td>Content Here</td>
+                <td>Content Here</td>
+                <td>Content Here</td>
+            </tr>
+            <tr>
+                <td>Content Here</td>
+                <td>Content Here</td>
+                <td>Content Here</td>
+                <td>Content Here</td>
+            </tr>
+
+            <tr>
+                <td>Content Here</td>
+                <td>Content Here</td>
+                <td>Content Here</td>
+                <td>Content Here</td>
+            </tr>
+            <tr>
+                <td>Content Here</td>
+                <td>Content Here</td>
+                <td>Content Here</td>
+                <td>Content Here</td>
+            </tr>
+            <tr>
+                <td>Content Here</td>
+                <td>Content Here</td>
+                <td>Content Here</td>
+                <td>Content Here</td>
+            </tr>
+            </tbody>
+
+        </table>
+    </div>
+</div>
