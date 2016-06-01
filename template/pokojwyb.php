bb51c9524cccbd3d2615d8bfa6f6106c45950bfb
Agata Ciesielska <Agata Ciesielska>
2016-06-01 19:13:18 +0200

    k

---
+++ template/pokojwyb.php
@@ -0,0 +1,24 @@
+<div class="primary callout archive">
+    <div class="row large-10">
+        <h3><strong>Baza pokoi</strong></h3>
+        <table>
+            <thead>
+            <tr>
+
+                <th width="100">Ilość osób</th>
+                <th width="200">Opis</th>
+                <th width="100">Cena</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>Content Here</td>
+                <td>Content Here</td>
+                <td>Content Here</td>
+            </tr>
+
+            </tbody>
+
+        </table>
+    </div>
+</div>
