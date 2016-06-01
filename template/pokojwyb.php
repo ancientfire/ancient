4648ad337cd35da459ce3f554c21937e7b8e05a6
Agata Ciesielska <Agata Ciesielska>
2016-06-01 19:49:21 +0200

    .

--- template/pokojwyb.php
+++ template/pokojwyb.php
@@ -1,3 +1,8 @@
+<?php
+
+include 'config.php';
+
+echo '
 <div class="primary callout archive">
     <div class="row large-10">
         <h3><strong>Baza pokoi</strong></h3>
@@ -9,16 +14,25 @@
                 <th width="200">Opis</th>
                 <th width="100">Cena</th>
             </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <td>Content Here</td>
-                <td>Content Here</td>
-                <td>Content Here</td>
-            </tr>
+            </thead><tbody>';
 
+$query = "select * from pokoj left outer join typ_pokoju on pokoj.typ=typ_pokoju.typ left outer join rezerwacja_pokoju on pokoj.id_pokoju=rezerwacja_pokoju.id_pokoju left outer join rezerwacja on rezerwacja_pokoju.id_rez_pok=rezerwacja.id_rez_pok where pokoj.typ=3 and (not ('2016-06-11' between rezerwacja.data_przyjazdu and rezerwacja.data_wyjazdu) and not ('2016-06-11' between rezerwacja.data_przyjazdu and rezerwacja.data_wyjazdu) or rezerwacja_pokoju.id_pokoju is null) limit 1";
+$result = pg_query($query);
+
+while ($row = pg_fetch_row($result)){
+echo "
+            <tr>
+                <td>$row[2]</td>
+                <td>Chuj dupa i kamieni kupa.</td>
+                <td>$row[4]</td>
+            </tr>";
+		}
+pg_close($dbconn);
+echo '
             </tbody>
 
         </table>
     </div>
-</div>
+</div>';
+
+?>
