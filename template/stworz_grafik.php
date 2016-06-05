4ec1d3a81db76d4df6a08232b27553bcf2a938cb
Agata Ciesielska <Agata Ciesielska>
2016-06-05 14:27:18 +0200

    .

--- template/stworz_grafik.php
+++ template/stworz_grafik.php
@@ -19,7 +19,7 @@
                             </select> </th>
 
                         <th>Data wyjazdu:
-                            <input type="text" class="span2" name="dw" id="dpd2">
+                            <input type="text" class="span2" name="dw" id="dpd">
                         </th>
 
                     <th> Zmiana:
@@ -56,34 +56,24 @@
                         <input type="submit" name="zapisz" class="button rejestracja2" value="ZAPISZ GRAFIK" />
                     </div>
 
-                <script>
-                    // implementation of disabled form fields
-                    var nowTemp = new Date();
-                    var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
-                    var checkin = $('#dpd1').fdatepicker({
-                        onRender: function (date) {
-                            return date.valueOf() < now.valueOf() ? 'disabled' : '';
-                        }
-                    }).on('changeDate', function (ev) {
-                        if (ev.date.valueOf() > checkout.date.valueOf()) {
-                            var newDate = new Date(ev.date)
-                            newDate.setDate(newDate.getDate() + 1);
-                            checkout.update(newDate);
-                        }
-                        checkin.hide();
-                        $('#dpd2')[0].focus();
-                    }).data('datepicker');
-                    var checkout = $('#dpd2').fdatepicker({
-                        onRender: function (date) {
-                            return date.valueOf() <= checkin.date.valueOf() ? 'disabled' : '';
-                        }
-                    }).on('changeDate', function (ev) {
-                        checkout.hide();
-                    }).data('datepicker');
-                </script>
+
                     </table>
                 </div>
             </form>
         </div>
     </div>
+    
+                    <script>
+                    // implementation of disabled form fields
+                    var nowTemp = new Date();
+                    var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
+                    var checkin = $('#dpd').fdatepicker({
+                        onRender: function (date) {
+                            return date.valueOf() < now.valueOf() ? 'disabled' : '';
+                        }
+                    }).on('changeDate', function (ev) {
+                        checkin.hide();
+                    }).data('datepicker');
+                    
+                </script>
 
